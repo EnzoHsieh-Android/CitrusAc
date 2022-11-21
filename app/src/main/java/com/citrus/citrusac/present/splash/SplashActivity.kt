8011c9a15f3a390b92b77a127a506515a197b28e
Enzo <8yinpro@gmail.com>
2022-11-21 16:27:00 +0800

    11/21 - Fix bug with Moshi won't allow invisible property

---
+++ app/src/main/java/com/citrus/citrusac/present/splash/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.citrus.citrusac.present.splash
+
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.view.View
+import com.citrus.citrusac.databinding.ActivitySplashBinding
+import com.citrus.citrusac.present.main.MainActivity
+import com.citrus.util.base.BaseActivity
+import com.daimajia.androidanimations.library.Techniques
+import com.daimajia.androidanimations.library.YoYo
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : BaseActivity<ActivitySplashBinding>(ActivitySplashBinding::inflate) {
+    override fun initView() {
+        YoYo.with(Techniques.FadeIn).duration(3000)
+            .onStart {
+                binding.ivSplash.visibility = View.VISIBLE
+            }.onEnd {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }.playOn(binding.ivSplash)
+
+
+    }
+
+    override fun initObserve() = Unit
+
+}
