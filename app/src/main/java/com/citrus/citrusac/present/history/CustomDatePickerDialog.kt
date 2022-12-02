c0240a9eb8977cd1283de7cb152ca49cefa0be6a
Enzo <8yinpro@gmail.com>
2022-12-02 17:37:30 +0800

    12/02 - 歷史頁搜尋條件增加時段、設定頁顯示店家名稱

--- app/src/main/java/com/citrus/citrusac/present/history/CustomDatePickerDialog.kt
+++ app/src/main/java/com/citrus/citrusac/present/history/CustomDatePickerDialog.kt
@@ -1,32 +1,37 @@
 package com.citrus.citrusac.present.history
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.view.View
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 import com.citrus.citrusac.databinding.DailogDatePickerBinding
 import com.citrus.util.base.BaseDialogFragment
+import com.citrus.util.ext.toHourMinStr
 import com.citrus.util.ext.toLocalTime
+import com.citrus.util.onSafeClick
 import com.daimajia.androidanimations.library.Techniques
 import com.daimajia.androidanimations.library.YoYo
 import splitties.snackbar.snack
-import splitties.views.onClick
 import java.time.LocalDateTime
 import java.time.format.TextStyle
 import java.util.*
 
+@RequiresApi(Build.VERSION_CODES.O)
 @SuppressLint("SetTextI18n")
 class CustomDatePickerDialog(
     private var selectedDateTimes: List<LocalDateTime>,
-    private val datePickType: DatePickType = DatePickType.OnlyDate,
-    private val maxSelectedSize: Int = 3,
+    private val datePickType: DatePickType,
+    private val maxSelectedSize: Int? = null,
     private val listener: (List<LocalDateTime>) -> Unit,
 ) : BaseDialogFragment<DailogDatePickerBinding>(DailogDatePickerBinding::inflate, false) {
 
-
     var selectedDates = selectedDateTimes.map { it.toLocalDate() }.toMutableList()
 
-
     override fun initView() {
-        isCancelable = true
+        isCancelable = false
+        //isTablet
+        setWindowWidthPercent(0.65, 0.85)
 
         binding.apply {
 
@@ -43,28 +48,40 @@ class CustomDatePickerDialog(
                     tvStartTime.text = "00:00"
                     tvEndTime.text = "23:59"
                 }
-                DatePickType.DateAndTime -> Unit
+                DatePickType.DateAndTime -> {
+                    try {
+                        tvStartTime.text = selectedDateTimes.first().toHourMinStr()
+                        tvEndTime.text = selectedDateTimes.last().toHourMinStr()
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                        showError(e)
+                        return@apply
+                    }
+                }
             }
 
 
 
-            btnOK.onClick {
+            tvStartTime.onSafeClick {
+                showTimePickerDialog(it as TextView)
+            }
+
+            tvEndTime.onSafeClick {
+                showTimePickerDialog(it as TextView)
+            }
+
+
+            btnOK.onSafeClick {
                 if (selectedDates.isEmpty()) {
                     YoYo.with(Techniques.Shake).duration(1000).playOn(calendarView)
-                    binding.root.snack("尚未選擇區間")
-                    return@onClick
+                    binding.root.snack("尚未完成選曲")
+                    return@onSafeClick
                 }
 
-                if (selectedDates.size > maxSelectedSize) {
+                if (maxSelectedSize != null && selectedDates.size > maxSelectedSize) {
                     YoYo.with(Techniques.Shake).duration(1000).playOn(calendarView)
-
-                    binding.root.snack(
-                        String.format(
-                            "僅能查詢三日內門禁資訊",
-                            maxSelectedSize
-                        )
-                    )
-                    return@onClick
+                    binding.root.snack("日期所選數量已超過可查詢總天數")
+                    return@onSafeClick
                 }
 
 
@@ -72,52 +89,50 @@ class CustomDatePickerDialog(
                 if (selectedDateTimes.size == 1) {
                     selectedDateTimes.add(selectedDateTimes.first()) //加一個date來放結尾時間
                 }
-                selectedDateTimes[0] = selectedDateTimes.first()
-                    .with("${(tvStartTime.text.toString())}:00".toLocalTime())
-                selectedDateTimes[selectedDateTimes.size - 1] =
-                    selectedDateTimes.last().with("${(tvEndTime.text.toString())}:59".toLocalTime())
+                selectedDateTimes[0] = selectedDateTimes.first().with("${(tvStartTime.text.toString())}:00".toLocalTime())
+                selectedDateTimes[selectedDateTimes.size - 1] = selectedDateTimes.last().with("${(tvEndTime.text.toString())}:59".toLocalTime())
 
                 if (selectedDateTimes.first().isAfter(selectedDateTimes.last())) {
-                    binding.root.snack("輸入錯誤")
+                    binding.root.snack("參數錯誤")
                     YoYo.with(Techniques.Shake).duration(1000).playOn(calendarView)
-                    return@onClick
+                    return@onSafeClick
                 }
+
 
                 listener(selectedDateTimes)
                 dismiss()
             }
 
-            btnClose.onClick {
+            btnClose.onSafeClick {
                 dismiss()
             }
         }
     }
 
     private fun DailogDatePickerBinding.initDateText() {
-        tvStartTimeWeek.text =
-            selectedDates.first().dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault())
-        tvEndTimeWeek.text =
-            selectedDates.last().dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault())
-        tvStartDate.text = "${
-            selectedDates.first().month.getDisplayName(
-                TextStyle.SHORT,
-                Locale.getDefault()
-            )
-        } ${selectedDates.first().dayOfMonth}"
-        tvEndDate.text = "${
-            selectedDates.last().month.getDisplayName(
-                TextStyle.SHORT,
-                Locale.getDefault()
-            )
-        } ${selectedDates.last().dayOfMonth}"
+        tvStartTimeWeek.text = selectedDates.first().dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault())
+        tvEndTimeWeek.text = selectedDates.last().dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault())
+        tvStartDate.text = "${selectedDates.first().month.getDisplayName(TextStyle.SHORT, Locale.getDefault())} ${selectedDates.first().dayOfMonth}"
+        tvEndDate.text = "${selectedDates.last().month.getDisplayName(TextStyle.SHORT, Locale.getDefault())} ${selectedDates.last().dayOfMonth}"
     }
 
+    private fun showError(e: Exception) {
+        //showAlertDialog("ERROR", e.toString())
+        dismiss()
+    }
 
-    override fun initAction() = Unit
+    override fun initAction() {
+    }
 
-    override fun clearMemory() = Unit
+    private fun showTimePickerDialog(v: TextView) {
+        TimePickerFragment(v.text.toString()) {
+            v.text = it
+        }.show(childFragmentManager, "timePicker")
+    }
 
+    override fun clearMemory() {
 
+    }
 }
 
 
